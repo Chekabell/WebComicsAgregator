2c8aabfafcb3b0d73386a94d9985c5f95a0da697
Chekabell <nikita.toporischev@mail.ru>
2025-09-19 10:11:57 +0500

    Fix routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 use App\Http\Controllers\ComicsControllerAPI;
 use App\Http\Controllers\CommentControllerAPI;
 use App\Http\Controllers\UserControllerAPI;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/api/comics', [ComicsControllerAPI::class, 'index']);
 Route::get('/api/comics/{comics}', [ComicsControllerAPI::class, 'show']);
