f337355f7faf4581eeea688e7f4caae73bb77f24
Chekabell <nikita.toporischev@mail.ru>
2025-09-19 10:10:12 +0500

    Added app.php

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+use App\Http\Controllers\ComicsControllerAPI;
+use App\Http\Controllers\CommentControllerAPI;
+use App\Http\Controllers\UserControllerAPI;
+
+Route::get('/api/comics', [ComicsControllerAPI::class, 'index']);
+Route::get('/api/comics/{comics}', [ComicsControllerAPI::class, 'show']);
+
+Route::get('/api/comments', [CommentControllerAPI::class, 'index']);
+Route::get('/api/comments/{comment}', [CommentControllerAPI::class, 'show']);
+
+Route::get('/api/users', [UserControllerAPI::class, 'index']);
+Route::get('/api/users/{user}', [UserControllerAPI::class, 'show']);
